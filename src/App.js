f0cbd8b0091219740b8e4f741922544fcb673f5b
Karen Huaulme <karen.huaulme@gmail.com>
2023-01-10 21:17:11 -0600

    started different pages

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
+import { Route, Routes, Link } from "react-router-dom";
+
 import React, { useState } from "react";
 //components
 import CodeSandbox from "./components/CodeSandbox";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
-import TutorialSections from "./TutorialSections";
+import TutorialSections from "./components/TutorialSections";
 import PDF from "./components/PDF";
-import AppPreview from "./components/AppPreview";
 
 import AdditionalResouces from "./components/AdditionalResouces";
+import Section1 from "./pages/Section1";
+import Home from "./pages/Home";
 
 function App() {
-  const [showSetUpPDF, setShowSetUpPDF] = useState(false);
+  // const [showSetUpPDF, setShowSetUpPDF] = useState(false);
   return (
-    <div h-screen bg-white>
-      <Header />
-      <div className="flex px-20">
-        <div className="flex flex-col pt-10">
-          <Intro
-            showSetUpPDF={showSetUpPDF}
-            setShowSetUpPDF={setShowSetUpPDF}
-          />
-          <hr />
-          {showSetUpPDF && <PDF />}
+    <>
+      <div> Routing Set-up</div>{" "}
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="section1" element={<Section1 />} />
+      </Routes>
+    </>
 
-          <hr />
-          <TutorialSections />
-          <hr />
-        </div>
-      </div>
+    // <div h-screen bg-white>
+    //   <Header />
+    //   <div className="flex px-20">
+    //     <div className="flex flex-col pt-10">
+    //       <Intro
+    //         showSetUpPDF={showSetUpPDF}
+    //         setShowSetUpPDF={setShowSetUpPDF}
+    //       />
+    //       <hr />
+    //       {showSetUpPDF && <PDF />}
 
-      <CodeSandbox appLink="https://codesandbox.io/embed/github/khuaulme/AtlasSearchWorkshop2022/tree/main/?fontsize=14&hidenavigation=1&theme=dark" />
-      <div className="flex px-20 items-center"></div>
+    //       <hr />
+    //       <TutorialSections />
+    //       <hr />
+    //     </div>
+    //   </div>
 
-      <AdditionalResouces />
-      <Footer />
-    </div>
+    //   <CodeSandbox appLink="https://codesandbox.io/embed/github/khuaulme/AtlasSearchWorkshop2022/tree/main/?fontsize=14&hidenavigation=1&theme=dark" />
+    //   <div className="flex px-20 items-center"></div>
+
+    //   <AdditionalResouces />
+    //   <Footer />
+    // </div>
   );
 }
 
