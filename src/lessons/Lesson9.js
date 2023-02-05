e6323b3ddb5012065e59becb2ef9b500203f6375
Karen Huaulme <karen.huaulme@gmail.com>
2023-02-05 16:57:05 +0100

    index reformatted page - need conclusion

--- src/lessons/Lesson9.js
+++ src/lessons/Lesson9.js
@@ -7,6 +7,10 @@ import SearchHeroIndex from "../images/SearchIndexHero2.png";
 import CodeSnippetsCopy from "../components/CodeSnippetsCopy";
 import KeyWord from "../components/KeyWord";
 import CodeReveal from "../components/CodeReveal";
+import Documents from "../images/Documents.png";
+import Mappings from "../images/Mappings.png";
+import TreasureMap from "../images/TreasureMap.png";
+import AnalyzerList from "../images/Analyzers.png";
 
 import { GiBookmarklet } from "react-icons/gi";
 import { FaCoins } from "react-icons/fa";
@@ -63,108 +67,113 @@ const Lesson9 = () => {
         {/****************BEGIN DIFFERENCE ****************************/}
 
         <div className="w-full flex flex-col">
-          <div className="flex pt-10 mx-auto justify-evenly items-center">
+          <div className="flex pt-10 mx-auto justify-evenly items-center px-20">
             <div className="w-1/2 ">
               To explain the differences between a typical MongoDB b-tree index
               and an inverted index used for search, let's walk through an
               example. Say we have a set of MongoDB documents that look like
-              this: ARROW________________
+              this: <span className="text-6xl">👉</span>
               <br></br>
-              <br></br> Each of these documents has an “_id” field as a unique
-              identifier, and an “s” field with some text.
+              <br></br> Each of these documents has an{" "}
+              <KeyWord type="variable">_id</KeyWord> field as a unique
+              identifier, and a <KeyWord type="variable">sentence</KeyWord>{" "}
+              field with some text.
             </div>
-            <div className="w-1/3">
+            <div className="w-1/3 bg-slateblue shadow-slate-400 shadow rounded">
               <img
-                src={Mapping}
+                src={Documents}
                 alt="mapping"
-                className="h-72 object-contain mx-auto shadow-slate-400 shadow my-4"
+                className="h-72 object-contain mx-auto  my-4 px-4 py-2"
               />
             </div>
           </div>
-        </div>
-        <div className="text-center h-1 bg-green-800 text-white text-2xl py-4 rounded mt-10 w-1/2 mx-auto"></div>
-        <div className="ROW2 flex font-barlow text-base w-full mt-6 mb-10 mx-auto justify-evenly space-x-20 text-left">
-          <div className="w-1/2">
-            To explain the differences between a typical MongoDB b-tree index
-            and an inverted index used for search, let's walk through an
-            example. Say we have a set of MongoDB documents that look like this:
-            <img
-              src={Mapping}
-              alt="mapping"
-              className="h-72 object-contain mx-auto shadow-slate-400 shadow my-4"
-            />
-            Each of these documents has an “_id” field as a unique identifier,
-            and an “s” field with some text. If I were to look through these
-            documents for the text:{" "}
-            <div className=" my-8 text-center mx-auto text-xl text-green-700 font-bold w-3/4 border-2 shadow-md shadow-slate-700 border-slate-700 rounded py-4 pr-12">
-              “It was the best of times, it was the worst of times.”
-              <div className="italic text-base text-right mt-2 text-gray-800">
-                A Tale of Two Cities, Charles Dickens
+          {/************END DOC INTRO **************/}
+          <div className="text-center h-1 bg-fuchsia-800 text-white  rounded mt-10 w-1/2 mx-auto"></div>
+          <div className="flex w-full space-x-16 justify-evenly mt-10 ">
+            <div className="w-1/3 text-center">
+              If I were to look through these documents for the text:{" "}
+              <div className=" my-8 text-center mx-auto px-4 text-xl text-green-700 font-bold w-3/4 border-2 shadow-md shadow-slate-700 border-slate-700 rounded py-4 pr-12">
+                “It was the best of times, it was the worst of times.”
+                <div className="italic text-base text-right mt-2 text-gray-800">
+                  A Tale of Two Cities, Charles Dickens
+                </div>
               </div>
-            </div>
-            Atlas Search would first break this down into these seven individual
-            terms <span className="italic text-green-700">( or tokens )</span>{" "}
-            {"  "}for our inverted index :{" "}
-            <div className="p-4 border  border-green-700 w-4/5 rounded mx-auto my-6">
-              {" "}
-              <img src={StandardAnalyzer} alt="analyzer" />
-            </div>
-          </div>
-
-          <div className="w-1/2 flex flex-col">
-            <div>
-              Armed with these new tokens:{" "}
-              <div className="my-4 mx-auto flex justify-evenly px-20 items-center spaxe-x-4">
+              Atlas Search would first break this down into these seven
+              individual terms{" "}
+              <span className="italic text-green-700">( or tokens )</span>{" "}
+              {"  "}for our inverted index :{" "}
+              <div className="p-4 border  border-green-700 w-3/4 rounded mx-auto my-6">
+                {" "}
+                <img src={StandardAnalyzer} alt="analyzer" />
+              </div>
+              Leaving us with:{" "}
+              <div className="my-4 mx-auto flex justify-evenly px-20 items-center space-x-4">
                 <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
                   it
                 </div>
-                {" - "}
+
                 <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
                   was
                 </div>
-                {" - "}
+
                 <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
                   the
                 </div>
-                {" - "}
+
                 <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
                   best
                 </div>
-                {" - "}
+
+                <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
+                  of
+                </div>
+
                 <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
                   times
                 </div>
-                {" - "}
+
                 <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
                   worst
                 </div>
               </div>{" "}
-              Atlas Search would map these back to the original MongoDB
-              documents’ _id fields. The word “it” can be found in document with
-              _id 4. Find “the” in documents 2, 3, 4, etc.
-              <img
-                src={Mapping}
-                alt="mapping"
-                className="h-72 object-contain mx-auto shadow-slate-400 shadow my-4"
-              />
-              So essentially, an inverted index is a mapping between terms and
-              the documents containing those terms. <br></br>
-              <br></br>Think of the inverted index as analogous to the index you
-              might find in the back of the book. Remember how book indexes
-              contain words or expressions and list the pages in the book where
-              they are found? 📖📚 Well, these inverted indexes use these terms
-              to point to the specific documents in your database.
             </div>
-            <div className="mt-4">
-              That’s why Inverted indexes are so much faster for text searches -
-              because you are not searching through every single document at
-              query time. You are instead querying the search index which was
-              mapped upon index creation. Then, following the roadmap with the
-              _id to the exact data document(s) is fast and easy.
+            <div className="w-1/4 mx-auto bg-slateblue py-6 my-auto">
+              <img src={Mappings} alt="map"></img>
+            </div>
+            <div className="w-1/3">
+              <div>
+                Armed with these new tokens, Atlas Search would map these back
+                to the original MongoDB documents’{" "}
+                <KeyWord type="variable">_id</KeyWord> fields.
+                <div className="ml-20 text-6xl my-4">👈</div>
+                The word <KeyWord type="word">"it"</KeyWord> can be found in
+                document with _id 4. Find <KeyWord type="word">"the"</KeyWord>{" "}
+                in documents 2, 3, 4, etc. So essentially, an inverted index is
+                a mapping between terms and the documents containing those
+                terms.{" "}
+                <div className=" mx-auto w-1/3">
+                  <img src={TreasureMap} alt="treasure" />
+                </div>
+                Think of the inverted index as analogous to the index you might
+                find in the back of the book. Remember how book indexes contain
+                words or expressions and list the pages in the book where they
+                are found?{" "}
+                <div className="my-1 text-6xl text-right pr-20">📖📚</div> Well,
+                these inverted indexes use these terms to point to the specific
+                documents in your database.
+              </div>
+              <div className="mt-4">
+                That’s why Inverted indexes are so much faster for text searches
+                - because you are not searching through every single document at
+                query time. You are instead querying the search index which was
+                mapped upon index creation. Then, following the roadmap with the
+                <KeyWord type="variable">_id</KeyWord> to the exact data
+                document(s) is fast and easy.
+              </div>
             </div>
           </div>
-          {/*********END 2ND COL ********************/}
         </div>
+
         <div className="text-center bg-fuchsia-800 text-white text-2xl py-4 rounded mt-10 w-1/2 mx-auto">
           What are Analyzers?
         </div>
@@ -172,9 +181,12 @@ const Lesson9 = () => {
         <div className="ROW2 flex font-barlow text-base w-full mt-6 mb-28 space-x-10 text-left">
           <div className="px-20 w-1/2 text-left">
             <div className=" mt-8">
-              To make data searchable, we can break it up into{" "}
-              <span className="text-teal-900 font-bold text-2xl "> tokens</span>{" "}
-              (or terms) to be indexed through a process called{" "}
+              The process of breaking up data into
+              <span className="text-teal-900 font-bold text-2xl ">
+                {" "}
+                tokens
+              </span>{" "}
+              (or terms) to be indexed is performed through a process called{" "}
               <span className=" font-bold text-2xl text-teal-900">
                 analysis
               </span>{" "}
@@ -183,13 +195,13 @@ const Lesson9 = () => {
                 {" "}
                 analyzers
               </span>
-              .
+              . Atlas Search offers the following analyzers:
             </div>
 
             <div className="flex justify-evenly mt-10 space-x-8">
-              <div className="p-4 border  border-green-700 w-4/5 rounded mx-auto">
+              <div className=" shadow shadow-slate-700 w-4/5 my-auto rounded mx-auto">
                 {" "}
-                {/* <img src={StandardAnalyzer} alt="analyzer" /> */}
+                <img src={AnalyzerList} alt="analyzer" />
               </div>
               <div className="flex flex-col mx-auto space-y-6">
                 {" "}
@@ -205,15 +217,30 @@ const Lesson9 = () => {
               </div>
             </div>
           </div>
-          <div className="w-1/2 flex flex-col text-base mt-8 ">
+          <div className="w-1/2 flex flex-col text-base my-10 ">
             <div>
-              Armed with your new tokens, you can now think of an{" "}
-              <span className="text-teal-800 font-bold"> inverted index</span>{" "}
-              as a glossary of tokens, like in the back of a book. You know how
-              the glossary contains words or expressions pointing to specific
-              pages? Well, search uses{" "}
-              <span className="text-teal-800 font-bold"> tokens</span> to point
-              specific data documents.
+              Analyzers are policies that combine a tokenizer, which extracts
+              tokens from text, with filters that you define.Different analyzers
+              produce different tokens to be mapped in the
+              <span className="text-teal-800 font-bold"> inverted index</span> .
+              The <KeyWord type="">Lucene.English</KeyWord> for instance removes
+              common words, or <KeyWord type="word">STOP WORDS</KeyWord> (i.e
+              "and", "the", "a", etc ) and stems the words to their root to
+              produce only the following tokens from Charles Dickens:
+              <div className="my-4 mx-auto flex justify-evenly px-20 items-center space-x-4">
+                <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
+                  best
+                </div>
+
+                <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
+                  time
+                </div>
+
+                <div className="text-xl py-2 px-4 border border-green-800 shadow font-bold shadow-green-900 rounded-xl">
+                  worst
+                </div>
+              </div>{" "}
+              which maps as follows:
             </div>
             <div className="w-full relative">
               {" "}
@@ -231,8 +258,8 @@ const Lesson9 = () => {
           </div>
         </div>
         {/*************END ROW2 ****************/}
-        <div className="flex flex-col w-full">
-          <div className="bg-sun font-lexend rounded shadow-2xl text-black text-center text-xl w-full py-5 px-10 mb-2 z-10">
+        <div className="flex flex-col w-full mt-10 ">
+          <div className="bg-sun font-lexend rounded shadow-2xl text-black text-center mt-10 text-xl w-full py-5 px-10 mb-2 z-10">
             Use the tool below to see how the different Lucene analyzers
             tokenize your data and affect your search results.
           </div>
