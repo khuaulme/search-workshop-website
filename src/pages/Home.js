9cd76444e38d1e9cdf95ef66a4caafd1bffebde7
Karen Huaulme <karen.huaulme@gmail.com>
2023-02-19 07:37:30 -0500

    added react-identifier on home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import DeviceIdentifier from "react-device-identifier";
 
 //components
 import CodeSandbox from "../components/CodeSandbox";
@@ -17,6 +18,16 @@ function Home() {
   return (
     <div className="h-screen bg-white">
       <Header />
+      <DeviceIdentifier isMobile={true} isTablet={true}>
+        <div className="flex justify-evenly px-4 py-2 bg-red-700 items-center  my-2">
+          {/* <img src={goalie} className="w-16" alt="outline"></img> */}
+          <div className=" text-white text-2xl text-center uppercase">
+            Best Viewed on Desktops
+          </div>
+
+          {/* <img src={player} className="w-16" alt="outline"></img> */}
+        </div>
+      </DeviceIdentifier>
       <div className="flex px-20">
         <div className="flex flex-col pt-10 text-lg">
           <Intro
