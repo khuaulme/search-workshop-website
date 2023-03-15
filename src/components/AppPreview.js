865fb3e88daf8c51d2b66f1b95827ff2b4272863
Karen Huaulme <karen.huaulme@gmail.com>
2023-03-15 15:47:44 -0500

    updated link to atlassearchmovies.com

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -1,4 +1,4 @@
-import Mflix from "../images/Mflix.png";
+import Mflix from "../images/MoviesScreen.png";
 
 const AppPreview = () => {
   return (
@@ -7,17 +7,19 @@ const AppPreview = () => {
         className="font-noto text-center text-3xl text-forestgreen"
         target="_blank"
         rel="noopener noreferrer"
-        href="https://atlassearchmovies-rsyxh.mongodbstitch.com/"
+        href="https://www.atlassearchmovies.com/"
       >
         www.atlassearchmovies.com
       </a>
       <img
         src={Mflix}
         alt="App"
-        className="mx-auto items-center shadow-xl rounded-xl w-full mt-4  shadow-black"
+        className="mx-auto items-center shadow-xl rounded-xl w-4/5 mt-4  shadow-black"
       />
     </div>
   );
 };
 
 export default AppPreview;
+
+//  href="https://atlassearchmovies-rsyxh.mongodbstitch.com/"
